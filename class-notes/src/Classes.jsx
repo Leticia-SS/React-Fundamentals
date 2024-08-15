7f43254b32a3c1819b1a7981c781551753ec2431
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-08-15 10:53:41 -0300

    build components

---
+++ class-notes/src/Classes.jsx
@@ -0,0 +1,12 @@
+function Classes() {
+    
+    const notes = "Aula 1"
+    
+    return(
+        <ul>
+            <li>{notes}</li>
+        </ul>
+    );
+}
+
+export default Classes
---
+++ class-notes/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            <h1>React Fundamentals</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Classes</a></li>
+                    <li><a href="#">Exercises</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    )
+}
+
+export default Header
